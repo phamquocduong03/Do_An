4b5cb0a5f8814f8b5c5ddfedaa5e8b17b7155d97
phamquocduong03 <135622655+phamquocduong03@users.noreply.github.com>
2023-12-07 03:29:02 +0700

    Update Controller.java

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -13,14 +13,6 @@ import java.sql.SQLException;
 
 public class Controller {
 
-    @FXML
-    private TableColumn<DataModel, String> baoHanhColumn;
-
-    @FXML
-    private TableColumn<DataModel, String> mayTinhColumn;
-
-    @FXML
-    private TableView<DataModel> tableView;
 
 
     private static final String URL = "jdbc:sqlserver://DUONG:1433;databaseName=QuanLyTiemNet;user=DUONG10112003;password=@Duong10112003";
@@ -28,37 +20,7 @@ public class Controller {
     public static String getUrl(){
         return URL;
     }
-   /* public void initialize() {
-        try {
-            // Thiết lập kết nối
-            Connection conn = DriverManager.getConnection(URL);
-            if (conn != null) {
-                System.out.println("Kết nối thành công!");
-                // Thực hiện truy vấn
-                ResultSet resultSet = conn.createStatement().executeQuery("SELECT MAMAY, BAOHANH FROM MAYTINH");
-
-                // Đổ dữ liệu từ ResultSet vào TableView
-                while (resultSet.next()) {
-                    String maMay = resultSet.getString("MAMAY");
-                    String baoHanh = resultSet.getString("BAOHANH");
-
-                    System.out.println("Ma May: " + maMay + ", Bao Hanh: " + baoHanh);
-                    // Tạo một đối tượng DataModel từ dữ liệu truy vấn
-                    DataModel data = new DataModel(maMay, baoHanh);
-                    // Thêm vào TableView
-                    tableView.getItems().add(data);
-                }
-
-                mayTinhColumn.setCellValueFactory(new PropertyValueFactory<>("maMay"));
-                baoHanhColumn.setCellValueFactory(new PropertyValueFactory<>("baoHanh"));
-
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }*/
-
+  
 
     
 }
