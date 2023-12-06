13a24d36a3248aa1e707a8d6af95f5fcd400508f
phamquocduong03 <phamquocduong10112003@gmail.com>
2023-12-07 03:45:23 +0700

    updates

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -20,35 +20,6 @@ public class Controller {
     public static String getUrl(){
         return URL;
     }
-   /* public void initialize() {
-        try {
-            // Thiết lập kết nối
-            Connection conn = DriverManager.getConnection(URL);
-            if (conn != null) {
-                System.out.println("Kết nối thành công!");
-                // Thực hiện truy vấn
-                ResultSet resultSet = conn.createStatement().executeQuery("SELECT MAMAY, BAOHANH FROM MAYTINH");
-
-                // Đổ dữ liệu từ ResultSet vào TableView
-                while (resultSet.next()) {
-                    String maMay = resultSet.getString("MAMAY");
-                    String baoHanh = resultSet.getString("BAOHANH");
-
-                    System.out.println("Ma May: " + maMay + ", Bao Hanh: " + baoHanh);
-                    // Tạo một đối tượng DataModel từ dữ liệu truy vấn
-                    DataModel data = new DataModel(maMay, baoHanh);
-                    // Thêm vào TableView
-                    tableView.getItems().add(data);
-                }
-
-                mayTinhColumn.setCellValueFactory(new PropertyValueFactory<>("maMay"));
-                baoHanhColumn.setCellValueFactory(new PropertyValueFactory<>("baoHanh"));
-
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
 
 }
 
